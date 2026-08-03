8757b624e463645f662987c890f78fa63ad88c94
deepukishore <deepu004.dk@gmail.com>
2026-08-03 14:52:16 +0530

    fix: logo

--- data/customers.py
+++ data/customers.py
@@ -22,7 +22,7 @@ CUSTOMER_BRANDS = {
     },
     "M&M - Mahindra and Mahindra": {
         "display_name": "Mahindra and Mahindra",
-        "logo": "images/customers/sml-mahindra.png",
+        "logo": "images/customers/mahindra.svg",
     },
     "VECV - Volvo Eicher Commercial Vehicles": {
         "display_name": "VE Commercial Vehicles",
